e310628aeab42d60e687e5476fce1fafb415df99
Ryniu <ryniu3001@gmail.com>
2016-10-08 12:34:49 +0200

    Dodanie GRASP

--- src/fc/put/to/algorithms/greedy/GraspGreedyCycle.java
+++ src/fc/put/to/algorithms/greedy/GraspGreedyCycle.java
@@ -4,6 +4,7 @@ import fc.put.to.Vertex;
 
 import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 /**
  * Created by marcin on 08.10.16.
@@ -18,5 +19,12 @@ public class GraspGreedyCycle extends GreedyCycle {
         Random r = new Random();
         int nearest = v.getCostList().get(r.nextInt(3)).getTarget();
         return vertices.get(nearest);
+    }
+
+    @Override
+    protected Connection chooseBestConnectionFromList(List<Connection> list){
+        list = list.stream().sorted((o1, o2) -> o1.cost - o2.cost == 0 ? 1 : o1.cost - o2.cost).limit(3).collect(Collectors.toList());
+        Random r = new Random();
+        return  list.get(r.nextInt(list.size() < 3 ? list.size() : 3));
     }
 }
