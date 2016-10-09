c6f4ef8b60307fff0cd730a2029b1f170b9f5deb
Maciej Kozłowski <mac229@interia.eu>
2016-10-10 01:31:07 +0200

    Dodana metoda rsyujaca NN

--- src/fc/put/to/Main.java
+++ src/fc/put/to/Main.java
@@ -1,6 +1,7 @@
 package fc.put.to;
 
-import fc.put.to.algorithms.greedy.GraspGreedyCycle;
+import fc.put.to.algorithms.greedy.Drawer;
+import fc.put.to.algorithms.nn.GraspNearestNeighbor;
 import fc.put.to.algorithms.nn.NearestNeighbor;
 
 import java.util.List;
@@ -11,14 +12,17 @@ public class Main {
         List<Vertex> vertexList = Parser.readFile();
 /*        GreedyCycle gc = new GreedyCycle(vertexList);
         gc.run();
-*/
         GraspGreedyCycle gc = new GraspGreedyCycle(vertexList);
         gc.run();
+*/
 
         NearestNeighbor nearestNeighbor = new NearestNeighbor(vertexList);
         nearestNeighbor.run();
 
-        //Drawer drawer = new Drawer();
-        //drawer.draw(gc.getBestIncidenceList());
+        GraspNearestNeighbor graspNearestNeighbor = new GraspNearestNeighbor(vertexList);
+        graspNearestNeighbor.run();
+
+        Drawer drawer = new Drawer();
+        drawer.drawNN(nearestNeighbor.getBestSolution());
     }
 }
--- src/fc/put/to/algorithms/greedy/Drawer.java
+++ src/fc/put/to/algorithms/greedy/Drawer.java
@@ -24,25 +24,25 @@ public class Drawer {
     private Graph graph;
     private Integer edId = 0;
 
-    public Drawer(){
+    public Drawer() {
         vertices = new ArrayList<>();
     }
 
-    public void draw(List<List<Vertex>> incidenceList){
+    public void draw(List<List<Vertex>> incidenceList) {
         readFileWithCoordinates();
         this.graph = new MultiGraph("Hello World!");
         this.vertices.forEach(this::drawNewVertex);
         Vertex from = incidenceList.stream().filter(vertices1 -> vertices1.size() == 2).findFirst().get().get(0);
         Vertex previous = from;
 
-        for (int i=0; i<50; i++) {
+        for (int i = 0; i < Constants.LENGHT_OF_SOLUTION; i++) {
             System.out.print(from + " ");
-            Vertex to ;
+            Vertex to;
             if (incidenceList.get(from.getId()).get(0).getId() != previous.getId())
                 to = incidenceList.get(from.getId()).get(0);
             else
                 to = incidenceList.get(from.getId()).get(1);
-            drawEdges(from,to);
+            drawEdges(from, to);
             previous = from;
             from = to;
         }
@@ -53,19 +53,38 @@ public class Drawer {
         viewer.disableAutoLayout();
     }
 
-    private void drawNewVertex(VertexWithCoord v){
+    public void drawNN(List<Vertex> bestSolution) {
+        readFileWithCoordinates();
+        this.graph = new MultiGraph("Hello World!");
+        this.vertices.forEach(this::drawNewVertex);
+
+        Vertex from = bestSolution.get(0);
+        for (int i = 1; i < Constants.LENGHT_OF_SOLUTION + 1; i++) {
+            System.out.print(from + " ");
+            Vertex to = bestSolution.get(i);
+            drawEdges(from, to);
+            from = to;
+        }
+        graph.addAttribute("ui.stylesheet", "node { size: 7px; z-index: 0;}");
+        graph.addAttribute("ui.stylesheet", "node { text-style: bold; text-size: 12;}");
+
+        Viewer viewer = graph.display();
+        viewer.disableAutoLayout();
+    }
+
+    private void drawNewVertex(VertexWithCoord v) {
         Node n = this.graph.addNode(v.id.toString());
         n.setAttribute("x", v.x);
         n.setAttribute("y", v.y);
         n.addAttribute("ui.label", v.id.toString());
     }
 
-    private void drawEdges(Vertex from, Vertex to){
+    private void drawEdges(Vertex from, Vertex to) {
         Edge e = graph.addEdge(edId.toString(), from.getId(), to.getId());
         edId++;
     }
 
-    private void readFileWithCoordinates(){
+    private void readFileWithCoordinates() {
         File file = new File(Constants.FILE_PATH_TSP);
         try (Stream<String> stream = Files.lines(file.toPath())) {
 
@@ -76,11 +95,11 @@ public class Drawer {
         }
     }
 
-    private class VertexWithCoord{
+    private class VertexWithCoord {
 
-        public VertexWithCoord(String lineOfFile){
+        public VertexWithCoord(String lineOfFile) {
             String[] line = lineOfFile.split(" ");
-            this.id = Integer.parseInt(line[0])-1;
+            this.id = Integer.parseInt(line[0]) - 1;
             this.x = Integer.parseInt(line[1]);
             this.y = Integer.parseInt(line[2]);
         }
