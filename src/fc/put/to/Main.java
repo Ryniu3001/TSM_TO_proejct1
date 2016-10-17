530ac2b58c15dd69d16f6b27e9073f80bae550b4
Maciej Kozłowski <mac229@interia.eu>
2016-10-18 00:56:42 +0200

    Refaktor Main

--- src/fc/put/to/Main.java
+++ src/fc/put/to/Main.java
@@ -4,6 +4,9 @@ import fc.put.to.algorithms.greedy.GraspGreedyCycle;
 import fc.put.to.algorithms.greedy.GreedyCycle;
 import fc.put.to.algorithms.local.LSResult;
 import fc.put.to.algorithms.local.LocalSearch;
+import fc.put.to.algorithms.nn.GraspNearestNeighbor;
+import fc.put.to.algorithms.nn.NearestNeighbor;
+import fc.put.to.algorithms.random.RandomSolution;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -13,39 +16,64 @@ public class Main {
     public static void main(String[] args) {
         List<Vertex> vertexList = Parser.readFile();
 
+        //nn(vertexList);
+        //nnGrasp(vertexList);
+
+        //greedyCycle(vertexList);
+        //greedyCycleGrasp(vertexList);
+
+        //randomSolution(vertexList);
+
+        nnLocal(vertexList);
+    }
+
+    private static void nnLocal(List<Vertex> vertexList) {
+        NearestNeighbor nearestNeighbor = new NearestNeighbor(vertexList);
+        List<LSResult> gResult = vertexList.stream()
+                .map(v -> new LocalSearch(vertexList, nearestNeighbor.findSolution(v)).run())
+                .collect(Collectors.toList());
+        printLCResult(gResult);
+    }
+
+    private static void randomSolution(List<Vertex> vertexList) {
+        RandomSolution randomSolution = new RandomSolution(vertexList);
+        randomSolution.run();
+    }
+
+    private static void nnGrasp(List<Vertex> vertexList) {
+        GraspNearestNeighbor graspNearestNeighbor = new GraspNearestNeighbor(vertexList);
+        graspNearestNeighbor.run();
+    }
+
+    private static void nn(List<Vertex> vertexList) {
+        NearestNeighbor nearestNeighbor = new NearestNeighbor(vertexList);
+        nearestNeighbor.run();
+    }
+
+    private static void greedyCycleGrasp(List<Vertex> vertexList) {
+        System.out.println("GRASP Greedy Cycle:");
+        GraspGreedyCycle ggc = new GraspGreedyCycle(vertexList);
+        ggc.run();
+
+        List<LSResult> gResult = vertexList.stream()
+                .map(v -> new LocalSearch(vertexList, GreedyCycle.incidenceToCycleList(ggc.getAllIncidenceMap().get(v), v)).run())
+                .collect(Collectors.toList());
+        printLCResult(gResult);
+    }
+
+    private static void greedyCycle(List<Vertex> vertexList) {
         System.out.println("Greedy Cycle:");
         GreedyCycle gc = new GreedyCycle(vertexList);
         gc.run();
+
         List<LSResult> result = vertexList.stream()
                 .map(v -> new LocalSearch(vertexList, GreedyCycle.incidenceToCycleList(gc.getAllIncidenceMap().get(v), v)).run())
                 .collect(Collectors.toList());
 
         printLCResult(result);
-        System.out.println();
-        //Local Search for GRASP GC
-        System.out.println("GRASP Greedy Cycle:");
-        GraspGreedyCycle ggc = new GraspGreedyCycle(vertexList);
-        ggc.run();
-        List<LSResult> gResult = vertexList.stream()
-                .map(v -> new LocalSearch(vertexList, GreedyCycle.incidenceToCycleList(ggc.getAllIncidenceMap().get(v), v)).run())
-                .collect(Collectors.toList());
-        printLCResult(gResult);
-        System.out.println();
-
-/*      NearestNeighbor nearestNeighbor = new NearestNeighbor(vertexList);
-        nearestNeighbor.run();
-
-       GraspNearestNeighbor graspNearestNeighbor = new GraspNearestNeighbor(vertexList);
-        graspNearestNeighbor.run();*/
-
-
-        //new RandomSolution(vertexList).run();
-/*
-        Drawer drawer = new Drawer();
-        drawer.drawNN(gResult.getCycle());*/
     }
 
-    private static void printLCResult(List<LSResult> list){
+    private static void printLCResult(List<LSResult> list) {
         System.out.println("Local Search:");
         System.out.println("Min: " + list.stream().min((o1, o2) -> o1.getCost() - o2.getCost()).get());
         System.out.println("Avg: " + list.stream().mapToInt(LSResult::getCost).average().getAsDouble());
